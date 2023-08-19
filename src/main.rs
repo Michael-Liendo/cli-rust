0f414106caf20ff1deb75cadfa051a3406b07ac9
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-18 20:06:03 -0400

    feat: configure clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
